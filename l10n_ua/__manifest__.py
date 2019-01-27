d2c3eb343cb8ab08a3d2053fafb5a7557c2bca65
Winepro <46692536+Winepro@users.noreply.github.com>
2019-01-27 08:42:36 +0200

    [ADD] app l10n_ua

---
+++ l10n_ua/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014-2018 CodUP (<http://codup.com>).
+#
+##############################################################################
+
+{
+    'name': 'Ukraine - Accounting',
+    'version': '3.4',
+    'category': 'Localization',
+    'description': """
+This is the base module to manage the accounting chart for Ukraine in Odoo.
+    """,
+    'author': 'CodUP',
+    'website': 'http://codup.com',
+    'license': 'AGPL-3',
+    'depends': ['account'],
+    'demo': [],
+    'data': [
+        'data/account_chart.xml',
+        'data/account.account.template.csv',
+        'data/account_chart_template.xml',
+        'data/account_chart_template_data.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'sequence': 1,
+    'installable': True,
+}
